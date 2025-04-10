ee70b0568f6c9d257db1df514c733773945a66db
Darya <zykovadara31@gmail.com>
2025-04-10 12:19:16 +0300

    update gitignore

--- KnowledgeApp/KnowledgeApp.DataAccess/obj/Debug/net8.0/KnowledgeApp.DataAccess.AssemblyInfo.cs
+++ KnowledgeApp/KnowledgeApp.DataAccess/obj/Debug/net8.0/KnowledgeApp.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KnowledgeApp.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589a2ecc6fe5915904535e6bc3c6f1d170390c18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f403fb8120ecc6234f20c64118c35a581c5d171")]
 [assembly: System.Reflection.AssemblyProductAttribute("KnowledgeApp.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KnowledgeApp.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
