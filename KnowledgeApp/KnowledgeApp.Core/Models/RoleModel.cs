194992928277451042c0bee5766234187a93cee6
Qwester471 <80134273+Qwester471@users.noreply.github.com>
2025-04-16 01:18:38 +0300

    added StatusModel and RoleModel

---
+++ KnowledgeApp/KnowledgeApp.Core/Models/RoleModel.cs
@@ -0,0 +1,18 @@
+﻿namespace KnowledgeApp.Core.Models;
+
+public class RoleModel
+{
+    public int Id { get; set; }
+
+    public string RoleName { get; set; } = null!;
+
+    public RoleModel(int id, string rolename)
+    {
+        Id = id;
+        RoleName = rolename;
+    }
+    public RoleModel(string rolename)
+    {
+        RoleName = rolename;
+    }
+}
---
+++ KnowledgeApp/KnowledgeApp.Core/Models/StatusModel.cs
@@ -0,0 +1,18 @@
+﻿namespace KnowledgeApp.Core.Models;
+
+public class StatusModel
+{
+    public int Id { get; set; }
+
+    public string StatusName { get; set; } = null!;
+
+    public StatusModel(int id, string statusname)
+    {
+        Id = id;
+        StatusName = statusname;
+    }
+    public StatusModel(string statusname)
+    {
+        StatusName = statusname;
+    }
+}
