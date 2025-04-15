8d3603a2a29ff74374d88f61f8edb334fff4bc43
Qwester471 <80134273+Qwester471@users.noreply.github.com>
2025-04-16 01:19:48 +0300

    added RoleService and StatusService

---
+++ KnowledgeApp/KnowledgeApp.Application/Services/RoleService.cs
@@ -0,0 +1,46 @@
+﻿using KnowledgeApp.DataAccess.Repositories;
+using KnowledgeApp.Core.Models;
+
+namespace KnowledgeApp.Application.Services
+{
+    public class RoleService
+    {
+        private readonly RoleRepository _roleRepository;
+
+        public RoleService(RoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<List<RoleModel>> GetAll()
+        {
+            List<RoleModel> roles = await _roleRepository.GetAllRoles();
+            return roles;
+        }
+
+        public async Task<RoleModel> GetRoleById(int roleId)
+        {
+            RoleModel role = await _roleRepository.GetRoleById(roleId);
+            return role;
+        }
+
+        public async Task<RoleModel> CreateRole(RoleModel roleModel)
+        {
+            RoleModel createdRoleId = await _roleRepository.CreateRole(roleModel);
+
+            return createdRoleId;
+        }
+
+        public async Task<RoleModel> UpdateRole(RoleModel roleModel)
+        {
+            RoleModel updatedRoleModel = await _roleRepository.UpdateRole(roleModel);
+            return updatedRoleModel;
+        }
+
+        public async Task<bool> DeleteRole(int roleId)
+        {
+            bool result = await _roleRepository.DeleteRole(roleId);
+            return result;
+        }
+    }
+}
---
+++ KnowledgeApp/KnowledgeApp.Application/Services/StatusService.cs
@@ -0,0 +1,46 @@
+﻿using KnowledgeApp.DataAccess.Repositories;
+using KnowledgeApp.Core.Models;
+
+namespace KnowledgeApp.Application.Services
+{
+    public class StatusService
+    {
+        private readonly StatusRepository _statusRepository;
+
+        public StatusService(StatusRepository statusRepository)
+        {
+            _statusRepository = statusRepository;
+        }
+
+        public async Task<List<StatusModel>> GetAll()
+        {
+            List<StatusModel> statuses = await _statusRepository.GetAllStatuses();
+            return statuses;
+        }
+
+        public async Task<StatusModel> GetStatusById(int statusId)
+        {
+            StatusModel status = await _statusRepository.GetStatusById(statusId);
+            return status;
+        }
+
+        public async Task<StatusModel> CreateStatus(StatusModel statusModel)
+        {
+            StatusModel createdStatusId = await _statusRepository.CreateStatus(statusModel);
+
+            return createdStatusId;
+        }
+
+        public async Task<StatusModel> UpdateStatus(StatusModel statusModel)
+        {
+            StatusModel updatedStatusModel = await _statusRepository.UpdateStatus(statusModel);
+            return updatedStatusModel;
+        }
+
+        public async Task<bool> DeleteStatus(int statusId)
+        {
+            bool result = await _statusRepository.DeleteStatus(statusId);
+            return result;
+        }
+    }
+}
