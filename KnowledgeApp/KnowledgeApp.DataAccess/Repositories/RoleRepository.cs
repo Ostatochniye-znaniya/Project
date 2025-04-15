ea71913301079b419a49fd5ea266de4be0e4c31a
Qwester471 <80134273+Qwester471@users.noreply.github.com>
2025-04-16 01:17:08 +0300

    added RoleRepository and StatusRepository

---
+++ KnowledgeApp/KnowledgeApp.DataAccess/Repositories/RoleRepository.cs
@@ -0,0 +1,84 @@
+﻿using KnowledgeApp.DataAccess.Context;
+using KnowledgeApp.DataAccess.Entities;
+using KnowledgeApp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace KnowledgeApp.DataAccess.Repositories
+{
+    public class RoleRepository
+    {
+        private readonly KnowledgeTestDbContext _context;
+
+        public RoleRepository(KnowledgeTestDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<RoleModel> CreateRole(RoleModel roleModel)
+        {
+            var rolename = await _context.Roles.SingleOrDefaultAsync(f => f.RoleName == roleModel.RoleName);
+            if (rolename == null) throw new Exception("Такой роли не существует");
+            var roleEntity = new Role
+            {
+                RoleName = roleModel.RoleName,
+                Id = roleModel.Id,
+            };
+
+            await _context.Roles.AddAsync(roleEntity);
+            await _context.SaveChangesAsync();
+
+            RoleModel createdRole = new RoleModel(roleEntity.Id, roleEntity.RoleName);
+            return createdRole;
+        }
+        public async Task<List<RoleModel>> GetAllRoles()
+        {
+            //достаем данные из бд
+            var roleEntities = await _context.Roles
+                .AsNoTracking()
+                .ToListAsync();
+
+            // преобразуем entities в models
+            var roles = roleEntities
+                .Select(roleEntity =>
+                {
+                    var roleModel = new RoleModel(
+                        roleEntity.Id,
+                        roleEntity.RoleName);
+
+                    return roleModel;
+                })
+                .ToList();
+
+            return roles;
+        }
+        public async Task<RoleModel> GetRoleById(int roleId)
+        {
+            var roleEntity = await _context.Roles.SingleOrDefaultAsync(d => d.Id == roleId);
+            if (roleEntity == null) throw new Exception("Role с таким id не существует");
+            RoleModel role = new RoleModel(roleEntity.Id, roleEntity.RoleName);
+            return role;
+        }
+        public async Task<RoleModel> UpdateRole(RoleModel roleModel)
+        {
+            var roleEntity = await _context.Roles.SingleOrDefaultAsync(d => d.Id == roleModel.Id);
+            if (roleEntity == null) throw new Exception("Role с таким id не существует");
+
+            roleEntity.RoleName = roleModel.RoleName;
+            _context.SaveChanges();
+            RoleModel role = new RoleModel(roleEntity.Id, roleEntity.RoleName);
+            return role;
+        }
+        public async Task<bool> DeleteRole(int roleId)
+        {
+            var roleEntity = await _context.Roles.SingleOrDefaultAsync(d => d.Id == roleId);
+            if (roleEntity == null) throw new Exception("Role с таким id не существует");
+            _context.Remove(roleEntity);
+            _context.SaveChanges();
+
+            var role = await _context.Roles.SingleOrDefaultAsync(d => d.Id == roleId);
+            if (role == null) return true;
+            else return false;
+        }
+    }
+}
---
+++ KnowledgeApp/KnowledgeApp.DataAccess/Repositories/StatusRepository.cs
@@ -0,0 +1,84 @@
+﻿using KnowledgeApp.DataAccess.Context;
+using KnowledgeApp.DataAccess.Entities;
+using KnowledgeApp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace KnowledgeApp.DataAccess.Repositories
+{
+    public class StatusRepository
+    {
+        private readonly KnowledgeTestDbContext _context;
+
+        public StatusRepository(KnowledgeTestDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<StatusModel> CreateStatus(StatusModel statusModel)
+        {
+            var status = await _context.Statuses.SingleOrDefaultAsync(f => f.StatusName == statusModel.StatusName);
+            if (status == null) throw new Exception("Такого status не существует");
+            var statusEntity = new Status
+            {
+                StatusName = statusModel.StatusName,
+                Id = statusModel.Id
+            };
+
+            await _context.Statuses.AddAsync(statusEntity);
+            await _context.SaveChangesAsync();
+
+            StatusModel createdStatus = new StatusModel(statusEntity.Id, statusEntity.StatusName);
+            return createdStatus;
+        }
+        public async Task<List<StatusModel>> GetAllStatuses()
+        {
+            //достаем данные из бд
+            var statusEntities = await _context.Statuses
+                .AsNoTracking()
+                .ToListAsync();
+
+            // преобразуем entities в models
+            var statuses = statusEntities
+                .Select(statusEntity =>
+                {
+                    var statusModel = new StatusModel(
+                        statusEntity.Id,
+                        statusEntity.StatusName);
+
+                    return statusModel;
+                })
+                .ToList();
+
+            return statuses;
+        }
+        public async Task<StatusModel> GetStatusById(int statusId)
+        {
+            var statusEntity = await _context.Statuses.SingleOrDefaultAsync(d => d.Id == statusId);
+            if (statusEntity == null) throw new Exception("Status с таким id не существует");
+            StatusModel status = new StatusModel(statusEntity.Id, statusEntity.StatusName);
+            return status;
+        }
+        public async Task<StatusModel> UpdateStatus(StatusModel statusModel)
+        {
+            var statusEntity = await _context.Statuses.SingleOrDefaultAsync(d => d.Id == statusModel.Id);
+            if (statusEntity == null) throw new Exception("Status с таким id не существует");
+
+            statusEntity.StatusName = statusModel.StatusName;
+            _context.SaveChanges();
+            StatusModel status = new StatusModel(statusEntity.Id, statusEntity.StatusName);
+            return status;
+        }
+        public async Task<bool> DeleteStatus(int statusId)
+        {
+            var statusEntity = await _context.Statuses.SingleOrDefaultAsync(d => d.Id == statusId);
+            if (statusEntity == null) throw new Exception("Status с таким id не существует");
+            _context.Remove(statusEntity);
+            _context.SaveChanges();
+
+            var status = await _context.Statuses.SingleOrDefaultAsync(d => d.Id == statusId);
+            if (status == null) return true;
+            else return false;
+        }
+    }
+}
